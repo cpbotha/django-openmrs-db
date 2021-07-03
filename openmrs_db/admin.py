22b558a54ef06c7a536f87d50712816cdcfc750e
Charl P. Botha <cpbotha@vxlabs.com>
2021-07-03 23:18:51 +0200

    Add Visit to the admin

--- openmrs_db/admin.py
+++ openmrs_db/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Concept, Obs, Patient, Person, PersonName
+from .models import Concept, Obs, Patient, Person, PersonName, Visit
 
 admin.site.register(Concept)
 admin.site.register(Obs)
 admin.site.register(Patient)
 admin.site.register(Person)
+admin.site.register(Visit)
 

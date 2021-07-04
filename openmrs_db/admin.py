225a68f3d4c75a36f4adeb2b61070bf5fab8ec7d
Charl P. Botha <cpbotha@vxlabs.com>
2021-07-04 20:54:26 +0200

    Add comment-out liquibase migration tables

--- openmrs_db/admin.py
+++ openmrs_db/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
 from .models import Concept, Obs, Patient, Person, PersonName, Visit
+#from .models import Liquibasechangelog
 
 admin.site.register(Concept)
 admin.site.register(Obs)
 admin.site.register(Patient)
 admin.site.register(Person)
 admin.site.register(Visit)
+
+# this is only to see what openmrs liquibase migrations have been done
+#admin.site.register(Liquibasechangelog)
 

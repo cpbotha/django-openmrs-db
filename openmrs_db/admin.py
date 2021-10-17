b3d60efd3c08881a2fca21d33482e8b68dcff4f5
Charl P. Botha <cpbotha@vxlabs.com>
2021-10-17 14:36:40 +0200

    Add EncounterType and Users to admin

--- openmrs_db/admin.py
+++ openmrs_db/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 
-from .models import Concept, Obs, Patient, Person, PersonName, Visit
+from .models import Concept, EncounterType, Obs, Patient, Person, PersonName, Users, Visit
 #from .models import Liquibasechangelog
 
 admin.site.register(Concept)
+admin.site.register(EncounterType)
 admin.site.register(Obs)
 admin.site.register(Patient)
 admin.site.register(Person)
 admin.site.register(Visit)
+admin.site.register(Users)
 
 # this is only to see what openmrs liquibase migrations have been done
 #admin.site.register(Liquibasechangelog)
